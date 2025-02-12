2584c4d4b9ac7478ae77e0ca976d48fb2c5f8080
Tonmoy Ghosh <tghosh@crimson.ua.edu>
2025-02-12 09:48:33 -0600

    Update run_start_the_container.bat
    Cleaning the code

--- run_start_the_container.bat
+++ run_start_the_container.bat
@@ -7,12 +7,8 @@ set workername=noisy_nn
 ::Delete previous container if exists
 docker rm -f %workername%
 
-::copy ..\lib_jitai\jitai_config.ini .
-::mkdir lib_jitai
-::copy ..\lib_jitai\*.py .\lib_jitai
-
 ::Build and run image:
 docker build  --no-cache -t %workername% .
 
 ::docker run --cpus=2 --gpus all -it --shm-size=30g --memory-swap "80g" --memory="40g" --name %workername% -d -v "%cd%":/workercode --network jitaiNetwork %workername%
-docker run --cpus=2 --gpus all -e TZ=America/Chicago -it --shm-size=30g --memory-swap "80g" --memory="40g" --name %workername% -d -v "%cd%":/workercode  -v jitai-img-vol:/jitairootdir --network jitaiNetwork %workername% 
+docker run --cpus=2 --gpus all -e TZ=America/Chicago -it --shm-size=30g --memory-swap "80g" --memory="40g" --name %workername% -d -v "%cd%":/workercode %workername% 
